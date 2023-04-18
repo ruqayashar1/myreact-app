94a11939994edceff754e24dc156ee8484d15299
rukia <ruqayashar1@gmail.com>
2023-04-18 10:45:11 +0300

    completed

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Topic: creating a basic react  application(Myreact-app)
 
 ## Steps followed:
-1. Install npm globally on the machine using "npm install -g"
+1. Install npm globally on the machine using "npm install -g".
 2.We create a basic react application called myreact-app.
 3. We cd into the myreact-app and open 'src/components/App.js' and write a code to display an image and a text description of it. 
 this is done by follwing the steps below:
--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import LibraryImage from "./bg.jpg";
 function App (){
   return (
     <div>
-      <h1>Library Image</h1>
+      <h1>Rukia's Library </h1>
    
-      <img src = {LibraryImage} alt = "Library Image"  height={200} weight={200}/>
-      <h1>This is my home library where i keep all the books that is relating to my study, motivational books and documentaries.</h1>
+      <img src = {LibraryImage} alt = "Library Image"  height={500} weight={500}/>
+      <p>This is my home library where i keep all the books that is relating to my study, motivational books and documentaries.</p>
+      <div>
+        <button onClick ={()=> alert('You clicked this button')}> Click Me</button>
+      </div>
     </div>
   );
 };
 export default App;
+
 
 
